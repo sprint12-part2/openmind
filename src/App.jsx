7d7fb5034e42f2f5f3892578b2bf547b170ba96b
chanki.kim <chanki.kim89@gmail.com>
2024-12-10 20:44:56 +0900

    Chore: 라우팅 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
 export default function App() {
-  return <div>OPENMIND</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
