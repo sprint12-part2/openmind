624b83ebc135c469f5dea0ed8b18cf1a766f34a5
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-11 11:40:32 +0900

    Feat(#29): svg icon 컴포넌트 작업 (#34)
    * Feat: SVG Icon 컴포넌트 작업
    
    * Chore: 샘플페이지에 표현

--- src/pages/sample/components/ChankiSample.jsx
+++ src/pages/sample/components/ChankiSample.jsx
@@ -1,3 +1,34 @@
+import { Icon } from "@components/Icon";
+import styles from "./ChankiSample.module.css";
+
 export default function ChankiSample() {
-  return <div>ChankiSample</div>;
+  return (
+    <div>
+      <div className={styles.flex}>
+        <Icon />
+        <Icon name="arrowDown" />
+        <Icon name="arrowDown" color="#fc0" />
+        <Icon name="arrowDown" color="red" />
+        <Icon name="arrowDown" size={40} color="var(--color-primary-300)" />
+        <Icon name="arrowDown" color="primary" />
+      </div>
+      <div className={styles.flex}>
+        <Icon name="arrowDown" />
+        <Icon name="arrowUp" />
+        <Icon name="arrowLeft" />
+        <Icon name="arrowRight" />
+        <Icon name="close" />
+        <Icon name="edit" />
+        <Icon name="facebook" />
+        <Icon name="kakao" />
+        <Icon name="link" />
+        <Icon name="message" />
+        <Icon name="more" />
+        <Icon name="person" />
+        <Icon name="reject" />
+        <Icon name="thumbsUp" />
+        <Icon name="thumbsDown" />
+      </div>
+    </div>
+  );
 }
