fdb3b28892d7a353df9dbc5036be1b6fbea25116
charlie95 <lus0905@gmail.com>
2024-12-11 19:39:52 +0900

    Feat(#32): Pagination 컴포넌트 개발 (#42)
    * Pagination 컴포넌트 개발
    
    * currentpage 상태관리 방식 변경
    
    * usestate 제거 및 Pagination 컴포넌트에 currentPage prop 추가

--- src/pages/sample/components/CharieSample.jsx
+++ src/pages/sample/components/CharieSample.jsx
@@ -1,3 +1,29 @@
+import React, { useState } from "react";
+import { Pagination } from "@components/Pagination";
+
 export default function CharieSample() {
-  return <div>CharieSample</div>;
+  // 현재 페이지 상태 관리
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalItems = 100; // 총 데이터 개수
+  const itemsPerPage = 8; // 한 페이지당 표시할 데이터 개수
+
+  // 페이지 변경 시 호출되는 함수
+  function handlePageChange(page) {
+    console.log(`현재 페이지: ${page}`); // 변경된 페이지 출력
+    setCurrentPage(page); // 현재 페이지 상태 업데이트
+  }
+
+  return (
+    <div>
+      <h1>Pagination Test</h1>
+      {/* Pagination 컴포넌트에 현재 페이지 상태 및 변경 함수 전달 */}
+      <Pagination
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage} // 현재 페이지 상태 전달
+        onPageChange={handlePageChange} // 페이지 변경 함수 전달
+      />
+    </div>
+  );
 }
