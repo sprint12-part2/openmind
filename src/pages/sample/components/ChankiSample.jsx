683fa0f55cae077568089667c65275b0b04ffdcc
chanki.kim <chanki.kim89@gmail.com>
2024-12-11 20:39:29 +0900

    Chore: 샘플페이지 작업

--- src/pages/sample/components/ChankiSample.jsx
+++ src/pages/sample/components/ChankiSample.jsx
@@ -1,10 +1,21 @@
 import { Icon } from "@components/Icon";
 import { Reaction } from "@components/Reaction";
 import styles from "./ChankiSample.module.css";
+import { Avatar } from "@components/Avatar/Avatar";
 
 export default function ChankiSample() {
   return (
     <div>
+      <div className={styles.flex}>
+        <Avatar
+          src="https://fastly.picsum.photos/id/502/200/200.jpg?hmac=c6mcZ5mcmjadIeDKaJClpvPz9R9-X9q6c0Un-n73Kv4"
+          name="방 사진"
+          size={120}
+        />
+        <div style={{ width: "80px" }}>
+          <Avatar src="error" />
+        </div>
+      </div>
       <div className={styles.flex}>
         <Reaction type="like" count={0} />
         <Reaction type="dislike" count={0} />
