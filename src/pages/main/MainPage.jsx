7b386087e0332f877b7e0223454930a5e49ac1ef
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-11 09:57:56 +0900

    Chore(#24): package install (#25)
    * Chore: 라이브러리 설치
    - dayjs
    - axios
    - clsx
    - react-toastify
    - react-lottie
    
    * Chore: jsconfig 추가
    - alias 자동 임포트되도록
    
    * Feat: 메인페이지에 임시로 Lottie img삽입

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,3 +1,13 @@
+import Lottie from "react-lottie";
+import LogoLottie from "@assets/img/common/logo.json";
+
 export default function MainPage() {
-  return <div>MainPage : Openmind</div>;
+  return (
+    <div>
+      MainPage : Openmind
+      <div>
+        <Lottie options={{ loop: true, autoplay: true, animationData: LogoLottie }} />
+      </div>
+    </div>
+  );
 }
