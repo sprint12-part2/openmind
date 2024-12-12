b3bc0cb3c0aac5ede48fa39e2e43c4cffe2a4f03
Namgyeon <getam101@naver.com>
2024-12-12 11:00:29 +0900

    Feat(#46): InputTextarea 공용컴포넌트 개발 (#47)
    * Style: inputFeild  단위 변경 및 style변수 지정
    
    * Feat: InputTextarea 공용 컴포넌트 개발
    
    * Chore: 주석 수정 테스트
    
    ---------
    
    Co-authored-by: chanki.kim <chanki.kim89@gmail.com>

---
+++ src/components/Input/InputTextarea.jsx
@@ -0,0 +1,20 @@
+import styles from "./InputTextarea.module.css";
+
+export function InputTextarea({
+  value,
+  onChange,
+  placeholder = "이름을 입력하세요",
+  rows = 6, // 텍스트 줄 수
+  maxLength = 500, // 최대 글자 수 입니다.
+}) {
+  return (
+    <textarea
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      rows={rows}
+      maxLength={maxLength}
+      className={styles.textarea}
+    />
+  );
+}
