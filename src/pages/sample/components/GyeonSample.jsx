2f3cc20c4b93b565fdadf904a993f3e842e08b91
Namgyeon <getam101@naver.com>
2024-12-13 00:08:44 +0900

    Style: UserCard 최소너비css 추가

--- src/pages/sample/components/GyeonSample.jsx
+++ src/pages/sample/components/GyeonSample.jsx
@@ -6,11 +6,16 @@ import { UserCard } from "@components/UserCard/UserCard";
 export default function GyeonSample() {
   return (
     <div className={styles.container}>
+      <InputField />
       <InputTextarea />
-      <section>
-        <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
-        <UserCard />
-      </section>
+      <div className={styles["section-container"]}>
+        <section className={styles.section}>
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+        </section>
+      </div>
     </div>
   );
 }
