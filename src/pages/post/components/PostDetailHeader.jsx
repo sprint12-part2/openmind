a69630170de6fd8314d2251ac2bf23c0689b1b81
chanki.kim <chanki.kim89@gmail.com>
2024-12-13 00:40:22 +0900

    Feat: PostDetail loader 추가

--- src/pages/post/components/PostDetailHeader.jsx
+++ src/pages/post/components/PostDetailHeader.jsx
@@ -1,6 +1,11 @@
+import { useLoaderData } from "react-router-dom";
 import styles from "./PostDetailHeader.module.css";
 
 export default function PostDetailHeader() {
+  //const data = useLoaderData(); // 이렇게 쓰셔도 되고, 분해하셔도 되욤
+  const { name, imageSource } = useLoaderData();
+  console.log(name);
+
   return (
     <div className={styles.container}>
       PostDetailHeader : 한솔님 작업구역 (답변, 질문페이지 공통헤더)
