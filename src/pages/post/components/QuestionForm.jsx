0674e5bc2fec79f0d087f4c70a3138cf028b4f97
chanki.kim <chanki.kim89@gmail.com>
2024-12-13 23:31:54 +0900

    Feat: 질문작성 컴포넌트 작업

---
+++ src/pages/post/components/QuestionForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useParams, useRouteLoaderData } from "react-router-dom";
+import useQuestion from "./useQuestion";
+import { Modal, InputTextarea, Avatar } from "@components/ui";
+import usePreventScroll from "@components/Modal/usePreventScroll";
+import styles from "./QuestionForm.module.css";
+
+export default function QuestionForm() {
+  const { id } = useParams();
+  const { name, imageSource } = useRouteLoaderData("post");
+  const [question, setQuestion] = useState("");
+  const [isModal, setIsModal] = useState(false);
+  const { mutate, isPending } = useQuestion(id);
+  const { preventScroll, allowScroll } = usePreventScroll();
+
+  const handleToggleModal = () => {
+    setQuestion("");
+    setIsModal(!isModal);
+    isModal ? allowScroll() : preventScroll();
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    mutate({
+      id,
+      question,
+    });
+    handleToggleModal();
+  }
+
+  return (
+    <>
+      <button type="button" onClick={handleToggleModal} className={styles.floatButton}>
+        질문 작성<em>하기</em>
+      </button>
+      {isModal && (
+        <Modal handleToggleModal={handleToggleModal} title="질문을 작성하세요" icon="message">
+          <div className={styles.user}>
+            <span className={styles.to}>To.</span>
+            <Avatar src={imageSource} alt={name} size={28} />
+            <span className={styles.name}>{name}</span>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <InputTextarea
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              placeholder="질문을 입력해주세요"
+            />
+            <button type="submit" disabled={!question || isPending} className={styles.button}>
+              질문 보내기
+            </button>
+          </form>
+        </Modal>
+      )}
+    </>
+  );
+}
