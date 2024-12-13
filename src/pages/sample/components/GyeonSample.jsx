c2ed088e03e490ccb54250c2c491da7fa9a88e8f
Namgyeon <getam101@naver.com>
2024-12-13 10:28:08 +0900

    Feat(#49): UserCard 공통 컴포넌트 구현 (#59)
    * Feat: UserCard 공통 컴포넌트 구현
    
    * Refactor: useState,useEffect 제거 후 css 반응형으로 크기 조절 구현
    
    * Style: css className들을 좀 더 간편하게 변경했습니다
    
    * Style: UserCard 최소너비css 추가
    
    * Test: UserCard 컴포넌트 sample 페이지에서 테스트
    
    * Refactor : 바렐패턴 작업

--- src/pages/sample/components/GyeonSample.jsx
+++ src/pages/sample/components/GyeonSample.jsx
@@ -6,6 +6,20 @@ export default function GyeonSample() {
     <div className={styles.container}>
       <InputField />
       <InputTextarea />
+      <div className={styles["section-container"]}>
+        <section className={styles.section}>
+          <UserCard
+            name={"남기연"}
+            imageSource={
+              "https://fastly.picsum.photos/id/502/200/200.jpg?hmac=c6mcZ5mcmjadIeDKaJClpvPz9R9-X9q6c0Un-n73Kv4"
+            }
+            questionCount={"10"}
+          />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+          <UserCard name={"남기연"} imageSource={undefined} questionCount={"10"} />
+        </section>
+      </div>
     </div>
   );
 }
