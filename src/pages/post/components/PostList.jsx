0152e62e515d411d0bdc1f6f7bc1fb46db8acd04
Charie95 <lus0905@gmail.com>
2024-12-13 22:29:56 +0900

    UserCard컴포넌트 연결, css 수정

--- src/pages/post/components/PostList.jsx
+++ src/pages/post/components/PostList.jsx
@@ -1,6 +1,7 @@
 import styles from "./PostList.module.css";
 import React, { useState, useEffect } from "react";
 import { fetchSubjects } from "@service/Subject";
+import { UserCard } from "@components/ui";
 
 export default function PostList() {
   const [subjects, setSubjects] = useState([]); // 질문자 목록 상태
@@ -9,7 +10,7 @@ export default function PostList() {
   useEffect(() => {
     async function loadSubjects() {
       try {
-        const data = await fetchSubjects(); // 기본값: page = 1, itemsPerPage = 8, sort = "time"
+        const data = await fetchSubjects(1, 8, "name"); // 기본값: page = 1, itemsPerPage = 8, sort = "time"
         setSubjects(data.results); // 받아온 데이터 설정
         console.log("불러온 데이터:", data.results); // 확인용 로그
       } catch (err) {
@@ -24,12 +25,12 @@ export default function PostList() {
   return (
     <div className={styles.container}>
       {subjects.map((subject) => (
-        <div key={subject.id}>
-          <p>name: {subject.name}</p>
-          <p>imageSource: {subject.imageSource}</p>
-          <p>questionCount: {subject.questionCount}</p>
-          <br />
-        </div>
+        <UserCard
+          key={subject.id}
+          name={subject.name}
+          imageSource={subject.imageSource || "default.jpg"} // 기본 이미지
+          questionCount={subject.questionCount}
+        />
       ))}
     </div>
   );
