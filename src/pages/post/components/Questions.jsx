028e09ea28fb6c103f5ae91284c0207561446cc5
chanki.kim <chanki.kim89@gmail.com>
2024-12-13 23:35:38 +0900

    Feat: 질문리스트 컴포넌트 작업

--- src/pages/post/components/Questions.jsx
+++ src/pages/post/components/Questions.jsx
@@ -1,5 +1,66 @@
+import { useRouteLoaderData } from "react-router-dom";
+import { Icon } from "@components/ui";
+import { FeedCard } from "@components/FeedCard";
+import emptyIcon from "@assets/img/common/icon_empty.svg";
 import styles from "./Questions.module.css";
+import useLike from "./useLike";
+import useAnswer from "./useAnswer";
 
-export default function Questions() {
-  return <div className={styles.container}>Questions & anwers : 찬기 작업페이지</div>;
+export default function Questions({ count, data, mode = "view" }) {
+  const userInfo = useRouteLoaderData("post");
+  const { create, update, remove, reject, isPending } = useAnswer();
+  const { mutate: handleLike } = useLike();
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Icon name="message" />
+        {count > 0 ? `${count}개의 질문이 있습니다.` : "아직 질문이 없습니다"}
+      </header>
+
+      {data.length ? (
+        <ul className={styles.list}>
+          {data.map((question) => {
+            return (
+              <li key={question.id}>
+                <FeedCard
+                  isPending={isPending}
+                  question={question}
+                  mode={mode}
+                  feedOwner={userInfo}
+                  onCreate={(content) =>
+                    create({ questionId: question.id, content, isRejected: "false" })
+                  }
+                  onUpdate={(content) =>
+                    update({
+                      answerId: question.answer?.id,
+                      content,
+                      isRejected: "false",
+                    })
+                  }
+                  onDelete={() =>
+                    remove({ questionId: question.id, answerId: question.answer?.id })
+                  }
+                  onReject={() =>
+                    reject({
+                      questionId: question.id,
+                      answerId: question.answer?.id,
+                      content: "reject",
+                      isRejected: true,
+                    })
+                  }
+                  onLike={() => handleLike({ id: question.id, type: "like" })}
+                  onDislike={() => handleLike({ id: question.id, type: "dislike" })}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className={styles.empty}>
+          <img src={emptyIcon} alt="아직 질문이 없습니다." />
+        </div>
+      )}
+    </div>
+  );
 }
