e2fc01bc62cc109fca04333db5bb5646c6d4c8ea
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-13 13:05:43 +0900

    Feat(#73): react query 셋팅 (#74)
    * Chore: react-query 패키지 설치
    
    * Feat: query client 설정, provider 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
