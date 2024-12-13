75bf1f00c9e5d4324a953e6b73c2ae38f39b9910
Namgyeon <getam101@naver.com>
2024-12-13 17:39:02 +0900

    Feat: 메인로고 반응형 구현

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,14 +1,15 @@
-import Lottie from "react-lottie";
-import LogoLottie from "@assets/img/common/logo.json";
+// import Lottie from "react-lottie";
+// import LogoLottie from "@assets/img/common/logo.json";
 import styles from "./MainPage.module.css";
+import MainLogo from "./components/MainLogo";
 
 export default function MainPage() {
   return (
     <div className={styles.container}>
-      MainPage : 기연님 작업페이지
-      <div>
+      <MainLogo />
+      {/* <div>
         <Lottie options={{ loop: true, autoplay: true, animationData: LogoLottie }} />
-      </div>
+      </div> */}
     </div>
   );
 }
