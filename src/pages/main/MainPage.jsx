2c434165571888565dcb805d91654302f82a1a8b
Namgyeon <getam101@naver.com>
2024-12-13 21:51:37 +0900

    Feat: 답변하러 가기 Link 구현(버튼 컴포넌트 완성되면 교체)

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -2,14 +2,21 @@
 // import LogoLottie from "@assets/img/common/logo.json";
 import styles from "./MainPage.module.css";
 import MainLogo from "./components/MainLogo";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
   return (
     <div className={styles.container}>
+      <div className={styles.header}>
+        {/* 버튼 컴포넌트 완성되기전까지 임시 */}
+        <Link to="/list">
+          <div className={styles.link}>답변하러 가기</div>
+        </Link>
+      </div>
       <MainLogo />
       {/* <div>
         <Lottie options={{ loop: true, autoplay: true, animationData: LogoLottie }} />
-      </div> */}
+      </div>  */}
     </div>
   );
 }
