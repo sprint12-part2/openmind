4d8c9c694b05cd70bbbc16c5e8abd4de08259fb5
chanki.kim <chanki.kim89@gmail.com>
2024-12-13 23:36:00 +0900

    Feat: 질문리스트 페이지, 답변페이지 작업

--- src/pages/post/PostAnswerPage.jsx
+++ src/pages/post/PostAnswerPage.jsx
@@ -1,13 +1,24 @@
 import { useParams } from "react-router-dom";
+import useQuestions from "./components/useQuestions";
+import PostMessage from "./components/PostMessage";
 import Questions from "./components/Questions";
 
 export default function PostAnswerPage() {
   const { id } = useParams();
+  const { count, results, ref, error, isLoading, isFetchingNextPage } = useQuestions(id);
+
+  if (error) {
+    return <PostMessage>질문을 가져오는중에 문제가 생겼습니다.</PostMessage>;
+  }
+
+  if (isLoading) {
+    return <PostMessage>질문을 가져오는 중입니다.</PostMessage>;
+  }
 
   return (
-    <div>
-      답변페이지
-      <Questions />
-    </div>
+    <>
+      <Questions count={count} data={results} mode="answer" />
+      <div ref={ref}>{isFetchingNextPage ? "더 불러오는 중입니다..." : ""}</div>
+    </>
   );
 }
