f0db183bc915bc47a55d84671871e928e46c4169
HANSOL <121593205+sol322@users.noreply.github.com>
2024-12-14 14:50:51 +0900

    Feat: 상세페이지 헤더 UI 스케치

--- src/pages/post/components/PostDetailHeader.jsx
+++ src/pages/post/components/PostDetailHeader.jsx
@@ -1,14 +1,36 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
+import { Avatar, Icon } from "@components/ui";
 import styles from "./PostDetailHeader.module.css";
+import logo from "/src/assets/img/common/logo.svg";
 
 export default function PostDetailHeader() {
   //const data = useLoaderData(); // 이렇게 쓰셔도 되고, 분해하셔도 되욤
   const { name, imageSource } = useLoaderData();
-  console.log(name);
 
   return (
-    <div className={styles.container}>
-      PostDetailHeader : 한솔님 작업구역 (답변, 질문페이지 공통헤더)
+    <div className={styles.header__wrapper}>
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={logo} alt="로고 이미지" />
+        </Link>
+      </div>
+      <div className={styles.avatar}>
+        <Avatar src={imageSource} alt={name + "님의 프로필 이미지"} />
+      </div>
+      <div className={styles.title}>
+        <h1>{name}</h1>
+      </div>
+      <div className={styles.sns}>
+        <button className={styles.sns__link}>
+          <Icon name="link" color="var(--color-white)" size="18" />
+        </button>
+        <button className={styles.sns__kakao}>
+          <Icon name="kakao" color="var(--color-black)" size="18" />
+        </button>
+        <button className={styles.sns__facebook}>
+          <Icon name="facebook" color="var(--color-white)" size="18" />
+        </button>
+      </div>
     </div>
   );
 }
