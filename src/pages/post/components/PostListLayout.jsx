00b780970d0c3b79b875d48c2c86dae42668b9e7
RINAHEO <90673296+RINAHEO@users.noreply.github.com>
2024-12-15 21:15:32 +0900

    Feat(#137) post list header  (#138)
    * Feat(#122): useFeed Context 생성 (방생성, 삭제 전역 관리) (#123)
    
    * Feat: useFeed Context 생성 (방생성, 삭제 전역 관리)
    
    * Refactor: 필요없는 try 구문 삭제
    
    * Feat: 현재 페이지 URL 복사 기능 개발 (#128)
    
    * Feat: 피드 리스트 페이지 레이아웃 구현
    
    ---------
    
    Co-authored-by: 김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
    Co-authored-by: HANSOL <121593205+sol322@users.noreply.github.com>

--- src/pages/post/components/PostListLayout.jsx
+++ src/pages/post/components/PostListLayout.jsx
@@ -4,11 +4,11 @@ import styles from "./PostListLayout.module.css";
 
 export default function PostListLayout() {
   return (
-    <div className={styles.container}>
-      <div>리나님, 피드 리스트페이지 레이아웃 설정하는곳</div>
+    <>
+      {/* 리나님, 피드 리스트페이지 레이아웃 설정하는곳 */}
       <PostListHeader />
 
       <Outlet />
-    </div>
+    </>
   );
 }
