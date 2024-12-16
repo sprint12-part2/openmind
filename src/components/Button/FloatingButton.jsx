3bcc0e58d051967f3c5f862a037f955bfaa6fcbe
LINAHEO <leeena94@naver.com>
2024-12-16 16:13:23 +0900

    Refactor: 버튼 수정중

--- src/components/Button/FloatingButton.jsx
+++ src/components/Button/FloatingButton.jsx
@@ -4,13 +4,29 @@ export function FloatingButton({
   onClick,
   children,
   className,
+  size = "md",
+  floating = true,
+  position = { top: "auto", right: "20px", bottom: "20px", left: "auto" },
   color = "primary",
   type = "button",
   disabled = false,
 }) {
   return (
     <button
-      className={clsx(styles.button, styles[color], disabled && styles.disabled, className)}
+      className={clsx(
+        styles.button,
+        styles[color],
+        styles[size],
+        disabled && styles.disabled,
+        floating && styles.floating,
+        className,
+      )}
+      style={{
+        top: floating ? position.top : "auto",
+        right: floating ? position.right : "auto",
+        bottom: floating ? position.bottom : "auto",
+        left: floating ? position.left : "auto",
+      }}
       type={type}
       onClick={onClick}
       disabled={disabled}
--- src/pages/sample/components/RinaSample.jsx
+++ src/pages/sample/components/RinaSample.jsx
@@ -1,14 +1,45 @@
 import { LinkButton, FloatingButton, ShareButton } from "@components/Button";
-
+import styles from "./RinaSample.module.css";
 export default function RinaSample() {
   return (
-    <div>
-      RinaSample
-      <LinkButton color="primary">질문하러 가기</LinkButton>
-      <LinkButton color="primary">질문하러 가기</LinkButton>
-      <LinkButton color="secondary">질문하러 가기</LinkButton>
-      <ShareButton color="#1877F2"></ShareButton>
-      <FloatingButton>질문 작성하기</FloatingButton>
+    <div className={styles.rinaPage__container}>
+      <div className={styles.rinaPage__content}>
+        LinkButton
+        <LinkButton color="primary">질문하러 가기</LinkButton>
+        <LinkButton color="secondary">질문하러 가기</LinkButton>
+        <LinkButton color="secondary" disabled={true}>
+          질문하러 가기
+        </LinkButton>
+      </div>
+      <div className={styles.rinaPage__content}>
+        FloatingButton
+        {/* 플로팅 버튼으로 사용할 때 floating 값을 true로 설정 
+            원하는 위치를 top,right,bottom,left 를 사용해서 위치 지정 가능 기본 위치는 right:20px, bottom:20px
+        */}
+        <FloatingButton size="md" floating={false}>
+          질문 작성하기
+        </FloatingButton>
+        <FloatingButton size="sm" floating={false}>
+          질문 작성하기
+        </FloatingButton>
+        <FloatingButton size="sm" floating={true} position={{ bottom: "50px", left: "50px" }}>
+          질문 작성하기
+        </FloatingButton>
+      </div>
+      <div className={styles.rinaPage__content}>
+        ShareButton
+        <ShareButton
+          color="var(--color-primary-400)"
+          icon="link"
+          iconColor="var(--color-white)"
+        ></ShareButton>
+        <ShareButton
+          color="var(--color-yellow)"
+          icon="kakao"
+          iconColor="var(--color-black)"
+        ></ShareButton>
+        <ShareButton color="#1877F2" icon="facebook" iconColor="var(--color-white)"></ShareButton>
+      </div>
     </div>
   );
 }
