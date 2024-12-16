40dbe144b31633439839d76fbc6bb1317270c2fd
Namgyeon <getam101@naver.com>
2024-12-16 17:24:55 +0900

    Feat: 이름 입력시 새로운 피드 생성, 답변하러 가기 버튼 교체

---
+++ src/pages/main/components/MainPageInputForm.jsx
@@ -0,0 +1,57 @@
+import { InputField } from "@components/ui.js";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./MainPageInputForm.module.css";
+import { LinkButton } from "@components/Button/index.js";
+import { useFeed } from "@context/FeedContext";
+import { Notify } from "@components/Toast";
+
+export default function MainPageInputForm() {
+  const [name, setName] = useState("");
+
+  const { createFeed, isLoading } = useFeed();
+
+  const navigate = useNavigate();
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Input값 유효성 검증
+      if (name) {
+        const data = await createFeed(name);
+
+        // localStorage에 저장
+        const savedFeeds = JSON.parse(localStorage.getItem("feeds")) || [];
+        localStorage.setItem("feeds", JSON.stringify([data, ...savedFeeds]));
+        // console.log(JSON.parse(localStorage.getItem("feeds")));
+
+        Notify({ type: "success", message: "피드를 생성했습니다." });
+        navigate(`/post/${data.id}/answer`);
+      } else {
+        Notify({ type: "error", message: "이름을 입력해주세요." });
+      }
+    } catch (e) {
+      if (e.response) {
+        // console.log(e.response.status);
+        // console.log(e.response.data);
+        Notify({ type: "error", message: "피드 생성을 실패했습니다." });
+      } else {
+        // console.log("리퀘스트가 실패했습니다");
+      }
+    }
+    setName("");
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <InputField value={name} onChange={handleNameChange} />
+      <LinkButton type="submit" color="secondary" disabled={isLoading}>
+        {isLoading ? "로딩 중 ..." : "질문 받기"}
+      </LinkButton>
+    </form>
+  );
+}
