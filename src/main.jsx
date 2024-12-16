d9a71b58e454dfd5a2b376328dca1097f6158ed7
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-16 09:35:51 +0900

    Refactor(#129): 질문, 답변 페이지 리팩토링 (훅 재정비) (#131)
    * Fix: FeedContext 개선
    - 삭제, 조회 인자로 문자를 받아도 숫자로 변환해서 비교
    
    * Refactor: 코드정리
    
    * Refactor: FeedCard내부로 들어가는 핸들러들을 모아서 훅으로 분리
    - 각페이지 도입부에서 핸들러들을 가져와서 프롭으로 내려주는 형식으로 변경
    - Questions 컴포넌트부터는 데이터 핸들링 로직이 없고, presentaion 컴포넌트로만 역활
    
    * Fix: Reaction 전달 인자 수정
    
    * Refactor: useImperativeHandle을 이용해서 Answer컴포넌트 내부로 isEdit state 이동
    
    * Refactor: FeedCard 개선
    - Reaction 분리
    - MoreMenu 분리
    
    * Feat: 피드 주인이면 답변하러가기 버튼 노출
    
    * Fix: css import 순서 조정
    - reset css가 app보다 밑이라, 스타일 오버라이딩이 안되는 현상 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "@assets/css/reset.css";
 import "@assets/css/global.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
