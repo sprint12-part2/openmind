0cb1432dd2418cfe6f2cc69e55a5cc87d1c22e0f
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-16 09:35:00 +0900

    Feat(#132): post페이지 loader 개선 (#133)
    * Feat: loader에 피드정보 조회 api 연결
    
    * Feat: Post페이지 접근시 보이는 hydrate fallback ui 작업

--- src/router.jsx
+++ src/router.jsx
@@ -11,7 +11,6 @@ import PostListLayout from "./pages/post/components/PostListLayout";
 import { loader as PostDetailLoader } from "./pages/post/loader/PostDetailLoader";
 import ProtectedRoute from "./pages/post/components/ProtectedRoute";
 import PostPageHydrateFallback from "./pages/post/components/PostPageHydrateFallback";
-import ProtectedRoute from "./pages/post/components/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
