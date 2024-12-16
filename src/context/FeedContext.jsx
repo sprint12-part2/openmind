8863010529772b9bd595a5a19fcb48ab2dca0ff8
chanki.kim <chanki.kim89@gmail.com>
2024-12-16 11:01:55 +0900

    Feat: 방문로그 저장, 클리어 컨텍스트 함수 작성 - 컨텍스트내 함수들을 usecallback처리해줌 (재생성 방지)

--- src/context/FeedContext.jsx
+++ src/context/FeedContext.jsx
@@ -1,11 +1,15 @@
 import { createSubject, deleteSubject } from "@service/Subject";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 
 const FeedContext = createContext();
 
 export const useFeed = () => useContext(FeedContext);
 
 export default function FeedContextProvider({ children }) {
+  const [visited, setVisited] = useState(() => {
+    const saved = localStorage.getItem("visited");
+    return saved ? JSON.parse(saved) : [];
+  });
   const [feeds, setFeeds] = useState(() => {
     const saved = localStorage.getItem("feeds");
     return saved ? JSON.parse(saved) : [];
@@ -16,28 +20,60 @@ export default function FeedContextProvider({ children }) {
     localStorage.setItem("feeds", JSON.stringify(feeds));
   }, [feeds]);
 
-  async function createFeed(name) {
+  useEffect(() => {
+    localStorage.setItem("visited", JSON.stringify(visited));
+  }, [visited]);
+
+  const createFeed = useCallback(async (name) => {
     setIsLoading(true);
 
     const response = await createSubject({ name });
     setFeeds((prev) => [response, ...prev]);
     setIsLoading(false);
     return response;
-  }
+  }, []);
 
-  async function removeFeed(feedId) {
+  const removeFeed = useCallback(async (feedId) => {
     setIsLoading(true);
 
     await deleteSubject(Number(feedId));
     setFeeds((prev) => prev.filter((feed) => feed.id !== feedId));
     setIsLoading(false);
-  }
+  }, []);
 
-  function hasFeed(feedId) {
-    return feeds.find((feed) => feed.id === Number(feedId));
-  }
+  const hasFeed = useCallback(
+    (feedId) => {
+      return feeds.find((feed) => feed.id === Number(feedId));
+    },
+    [feeds],
+  );
 
-  const ctxValue = { isLoading, feeds, createFeed, removeFeed, hasFeed };
+  const saveVisited = useCallback(
+    (feedData) => {
+      if (visited.find((feed) => feed.id === feedData.id)) return;
+
+      setVisited((prev) => {
+        const filterd = prev.filter((item) => item.id !== feedData.id);
+        return [feedData, ...filterd].slice(0, 4);
+      });
+    },
+    [feeds],
+  );
+
+  const clearVisited = useCallback(() => {
+    setVisited([]);
+  }, []);
+
+  const ctxValue = {
+    isLoading,
+    feeds,
+    createFeed,
+    removeFeed,
+    hasFeed,
+    visited,
+    saveVisited,
+    clearVisited,
+  };
 
   return <FeedContext.Provider value={ctxValue}>{children}</FeedContext.Provider>;
 }
