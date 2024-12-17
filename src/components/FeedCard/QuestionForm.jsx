311c5fce70d738f141d943ef8a467dda61d8f904
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-17 19:54:31 +0900

    Refactor: 공용 모달 수정으로 인한 코드 변경 (#191)

--- src/components/FeedCard/QuestionForm.jsx
+++ src/components/FeedCard/QuestionForm.jsx
@@ -1,24 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Modal, InputTextarea, Avatar, FloatingButton, LinkButton } from "@components/ui";
-import usePreventScroll from "@components/Modal/usePreventScroll";
 import styles from "./QuestionForm.module.css";
 
 export function QuestionForm({ feedOwner, onSubmit, isPending }) {
   const { name, imageSource } = feedOwner;
   const [content, setContent] = useState("");
-  const [isModal, setIsModal] = useState(false);
-  const { preventScroll, allowScroll } = usePreventScroll();
+  const modalRef = useRef(null);
 
   const handleToggleModal = () => {
     setContent("");
-    setIsModal(!isModal);
-    isModal ? allowScroll() : preventScroll();
+    modalRef.current.open();
   };
 
   async function handleSubmit(e) {
     e.preventDefault();
     onSubmit({ content });
-    handleToggleModal();
+    modalRef.current.close();
     window.scrollTo(0, 0);
   }
 
@@ -27,30 +24,33 @@ export function QuestionForm({ feedOwner, onSubmit, isPending }) {
       <FloatingButton type="button" onClick={handleToggleModal} className={styles.floatButton}>
         질문 작성<em>하기</em>
       </FloatingButton>
-      {isModal && (
-        <Modal handleToggleModal={handleToggleModal} title="질문을 작성하세요" icon="message">
-          <div className={styles.user}>
-            <span className={styles.to}>To.</span>
-            <Avatar src={imageSource} alt={name} size={28} />
-            <span className={styles.name}>{name}</span>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <InputTextarea
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              placeholder="질문을 입력해주세요"
-            />
-            <LinkButton
-              type="submit"
-              color="secondary"
-              disabled={!content || isPending}
-              className={styles.button}
-            >
-              질문 보내기
-            </LinkButton>
-          </form>
-        </Modal>
-      )}
+      <Modal
+        ref={modalRef}
+        handleToggleModal={handleToggleModal}
+        title="질문을 작성하세요"
+        icon="message"
+      >
+        <div className={styles.user}>
+          <span className={styles.to}>To.</span>
+          <Avatar src={imageSource} alt={name} size={28} />
+          <span className={styles.name}>{name}</span>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <InputTextarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="질문을 입력해주세요"
+          />
+          <LinkButton
+            type="submit"
+            color="secondary"
+            disabled={!content || isPending}
+            className={styles.button}
+          >
+            질문 보내기
+          </LinkButton>
+        </form>
+      </Modal>
     </>
   );
 }
