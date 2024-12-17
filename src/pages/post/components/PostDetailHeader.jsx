7d47c52461ead603f099b94d5dd78bb8ff3e796a
HANSOL <121593205+sol322@users.noreply.github.com>
2024-12-17 13:57:30 +0900

    Feat: 상세페이지 헤더 - 버튼 공용 컴포넌트로 교체

--- src/pages/post/components/PostDetailHeader.jsx
+++ src/pages/post/components/PostDetailHeader.jsx
@@ -1,9 +1,9 @@
 import { useLoaderData, Link } from "react-router-dom";
-import { Avatar, Icon } from "@components/ui";
+import { useEffect } from "react";
+import { Avatar, Icon, ShareButton } from "@components/ui";
 import { copyUrl, shareKakao, shareFacebook } from "@util/shareUtils";
 import styles from "./PostDetailHeader.module.css";
 import logo from "/src/assets/img/common/logo.svg";
-import { useEffect } from "react";
 
 export default function PostDetailHeader() {
   //const data = useLoaderData(); // 이렇게 쓰셔도 되고, 분해하셔도 되욤
@@ -32,15 +32,28 @@ export default function PostDetailHeader() {
         <h1>{name}</h1>
       </div>
       <div className={styles.sns}>
-        <button className={styles.sns__link} onClick={copyUrl}>
-          <Icon name="link" color="var(--color-white)" size="18" />
-        </button>
+        <ShareButton
+          color="var(--color-primary-400)"
+          icon="link"
+          iconColor="var(--color-white)"
+          onClick={copyUrl}
+        ></ShareButton>
+        {/* <ShareButton
+          color="var(--color-yellow)"
+          icon="kakao"
+          iconColor="var(--color-black)"
+          id="kakao-link-btn"
+          onClick={shareKakao}
+        ></ShareButton> */}
         <button className={styles.sns__kakao} id="kakao-link-btn" onClick={shareKakao}>
           <Icon name="kakao" color="var(--color-black)" size="18" />
         </button>
-        <button className={styles.sns__facebook} onClick={shareFacebook}>
-          <Icon name="facebook" color="var(--color-white)" size="18" />
-        </button>
+        <ShareButton
+          color="#1877F2"
+          icon="facebook"
+          iconColor="var(--color-white)"
+          onClick={shareFacebook}
+        ></ShareButton>
       </div>
     </div>
   );
