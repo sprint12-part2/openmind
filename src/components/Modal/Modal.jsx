e41f05bd8f3fb3b24e5f60dd07620c27972693e8
chanki.kim <chanki.kim89@gmail.com>
2024-12-17 19:15:53 +0900

    Fix: 모달 스크롤방지 수정(state -> ref로 변경)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import { Icon } from "@components/Icon";
 import styles from "./Modal.module.css";
-import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
 import usePreventScroll from "./usePreventScroll";
 import { createPortal } from "react-dom";
 
@@ -25,17 +25,35 @@ const container = {
 
 export const Modal = forwardRef(function Modal({ title, icon, children }, ref) {
   const [isOpen, setIsOpen] = useState(false);
-
-  const { preventScroll, allowScroll } = usePreventScroll();
+  const scrollPositionRef = useRef(0);
+  //const { preventScroll, allowScroll } = usePreventScroll();
 
   useEffect(() => {
     if (isOpen) {
-      preventScroll();
+      scrollPositionRef.current = window.scrollY;
+
+      document.body.style.position = "fixed";
+      document.body.style.width = "100%";
+      document.body.style.top = `-${scrollPositionRef.current}px`;
+      document.body.style.overflowY = "hidden";
     } else {
-      allowScroll();
+      document.body.style.position = "";
+      document.body.style.width = "";
+      document.body.style.top = "";
+      document.body.style.overflowY = "";
+      window.scrollTo(0, scrollPositionRef.current);
     }
-    return () => allowScroll();
-  }, [isOpen, preventScroll, allowScroll]);
+
+    return () => {
+      document.body.style.position = "";
+      document.body.style.width = "";
+      document.body.style.top = "";
+      document.body.style.overflowY = "";
+      if (scrollPositionRef.current) {
+        window.scrollTo(0, scrollPositionRef.current);
+      }
+    };
+  }, [isOpen]);
 
   useImperativeHandle(ref, function () {
     return {
