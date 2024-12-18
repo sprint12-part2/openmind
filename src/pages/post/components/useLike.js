680d95dace5d6fd7a6f5dd291c6a5de36bfb9e86
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-18 15:50:43 +0900

    Fix: 좋아요, 싫어요 query key 수정 (#230)

--- src/pages/post/components/useLike.js
+++ src/pages/post/components/useLike.js
@@ -9,7 +9,7 @@ export default function useLike(subjectId) {
     mutationFn: ({ questionId, type }) => addQuestionReaction(questionId, type),
     onMutate: async ({ questionId, type }) => {
       // 에러시 원복 데이터 생성
-      const prevData = queryClient.getQueriesData(["questions"]);
+      const prevData = queryClient.getQueriesData(["questions", subjectId]);
 
       // optimistic update (기존 데이터 이용해서 ui바로 업데이트)
       queryClient.setQueriesData(["questions", subjectId], (prev) => {
