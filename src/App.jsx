dedb4c7d052893f6234599b73304ed9a850b78d5
chanki.kim <chanki.kim89@gmail.com>
2024-12-18 15:45:11 +0900

    Fix: 루트레벨에서 react helmet 추가 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { router } from "./router";
 import FeedContextProvider from "@context/FeedContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toast } from "@components/Toast";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +12,10 @@ export default function App() {
     <FeedContextProvider>
       <QueryClientProvider client={queryClient}>
         <HelmetProvider>
+          <Helmet>
+            <title>OPENMIND</title>
+            <meta name="description" content="익명으로 소통하세요" />
+          </Helmet>
           <RouterProvider router={router} />
         </HelmetProvider>
         <Toast />
