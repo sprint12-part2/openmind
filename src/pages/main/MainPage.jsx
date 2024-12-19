e7a444d432f19376eb5d6ffa5fc97ce7ae8c3d34
Namgyeon <getam101@naver.com>
2024-12-19 17:28:27 +0900

    Refactor: 메인페이지 Lottie 클릭으로 재생제어 삭제 (#261)

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -30,7 +30,10 @@ export default function MainPage() {
         <MyFeeds ref={myFeedsModalRef} />
       </section>
       <div className={styles.lottie}>
-        <Lottie options={{ loop: true, autoplay: true, animationData: LogoLottie }} />
+        <Lottie
+          options={{ loop: true, autoplay: true, animationData: LogoLottie }}
+          isClickToPauseDisabled={true}
+        />
       </div>
     </div>
   );
