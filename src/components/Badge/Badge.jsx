2014d86ecd76ebbf787bb3218d622b1a4bcc4b9f
HANSOL <121593205+sol322@users.noreply.github.com>
2024-12-21 11:22:22 +0900

    Refactor: badge 리팩토링

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,35 +1,11 @@
+import clsx from "clsx";
 import styles from "./Badge.module.css";
 
-export function Badge({ status }) {
-  const badgeData = {
-    completed: {
-      label: "답변 완료",
-      color: "var(--color-primary-400)",
-      borderColor: "var(--color-primary-400)",
-    },
-    incomplete: {
-      label: "미답변",
-      color: "var(--color-gray-400)",
-      borderColor: "var(--color-gray-400)",
-    },
-  };
-
-  const badge = badgeData[status];
-  if (!badge) {
-    return null;
-  }
-
+export function Badge({ status = "incomplete" }) {
+  const css = clsx(styles.badge, styles[status]);
   return (
     <div>
-      <span
-        className={styles.badge}
-        style={{
-          color: badge.color,
-          borderColor: badge.borderColor,
-        }}
-      >
-        {badge.label}
-      </span>
+      <span className={css}>{status === "completed" ? "답변 완료" : "미답변"}</span>
     </div>
   );
 }
