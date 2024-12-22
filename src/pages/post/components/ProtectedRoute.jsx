784a9e6f67d86259f4ceb954a6645e55cc76d1fd
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2024-12-22 19:33:17 +0900

    Fix: useParams의 id를 number타입으로 변환후 필요한곳에 전달하도록 변경 (#297)

--- src/pages/post/components/ProtectedRoute.jsx
+++ src/pages/post/components/ProtectedRoute.jsx
@@ -5,8 +5,9 @@ import { useEffect } from "react";
 import { Navigate, useParams } from "react-router-dom";
 export default function ProtectedRoute({ children }) {
   const { id } = useParams();
+  const subjectId = Number(id);
   const { hasFeed } = useFeed();
-  const hasAccess = hasFeed(id);
+  const hasAccess = hasFeed(subjectId);
   useEffect(() => {
     // useeffect는 비동기라서
     // Navigate 컴포넌트가 반환되더라도, 스케쥴링되어서 실행되는점을 이용
