cca6cf328dac0bff5fd8c9344271beeb2af5c4c1
chanki.kim <chanki.kim89@gmail.com>
2024-12-23 09:21:41 +0900

    Fix: id parameter number타입으로 변환 누락된곳 수정

--- src/pages/post/loader/PostDetailLoader.js
+++ src/pages/post/loader/PostDetailLoader.js
@@ -2,7 +2,9 @@ import { fetchSubjectById } from "@service/Subject";
 
 export async function loader({ params }) {
   const { id } = params;
+  const subjectId = Number(id);
 
-  const data = await fetchSubjectById(id);
+  const data = await fetchSubjectById(subjectId);
+
   return data;
 }
